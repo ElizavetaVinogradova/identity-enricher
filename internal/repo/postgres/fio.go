6d0867728d4828f3c9c21cec9f6c94a23f55772f
liza <vinogradova.lizaveta@gmail.com>
2023-10-20 15:54:31 +0300

    small fixes for the repo

--- internal/repo/postgres/fio.go
+++ internal/repo/postgres/fio.go
@@ -5,7 +5,7 @@ import (
 	"identity-enricher/internal/service"
 
 	"github.com/jmoiron/sqlx"
-	log "github.com/sirupsen/logrus"
+	_ "github.com/lib/pq"
 )
 
 type FioRepository struct {
@@ -42,24 +42,20 @@ func (r *FioRepository) Close() {
 }
 
 type FioDataBaseDTO struct {
-	Name        string `json:"name"`
-	Surname     string `json:"surname"`
-	Patronymic  string `json:"patronymic"`
-	Age         int8   `json:"age"`
-	Gender      string `json:"gender"`
-	Nationality string `json:"nationality"`
+	Id          int64  `db:"id"`
+	Name        string `db:"name"`
+	Surname     string `db:"surname"`
+	Patronymic  string `db:"patronymic"`
+	Age         int8   `db:"age"`
+	Gender      string `db:"gender"`
+	Nationality string `db:"nationality"`
 }
 
 func (r *FioRepository) Create(fio service.Fio) error {
 	fioDB := mapServiceFioToFioDTO(fio)
-	query := "INSERT INTO fio (name, surname, patronymic, age, gender, nationality) VALUES (?, ?, ?, ?, ?, ?)"
-	stmt, err := r.db.Prepare(query)
-	if err != nil {
-		return fmt.Errorf("prepare statement: %w", err)
-	}
-	defer stmt.Close()
 
-	_, err = stmt.Exec(fioDB.Name, fioDB.Surname, fioDB.Patronymic, fioDB.Age, fioDB.Gender, fioDB.Nationality)
+	sqlStatement := `INSERT INTO fio (name, surname, patronymic, age, gender, nationality) VALUES ($1, $2, $3, $4, $5, $6)`
+	_, err := r.db.Exec(sqlStatement, fioDB.Name, fioDB.Surname, fioDB.Patronymic, fioDB.Age, fioDB.Gender, fioDB.Nationality)
 	if err != nil {
 		return fmt.Errorf("execute create statement: %w", err)
 	}
@@ -72,16 +68,13 @@ func (r *FioRepository) GetById(id int64) (service.Fio, error) {
 
 	var fioDB FioDataBaseDTO
 	err := r.db.Get(&fioDB, query, id)
-	log.Debugf("================statement got data : %s , id: %d", fmt.Sprintf("%v", fioDB), id)
 	if err != nil {
 		return service.Fio{}, fmt.Errorf("execute get statement: %w", err)
 	}
 
 	fio := mapFioDTOToFio(fioDB)
-	log.Debugf("================mapped DB tick to service tick : %s", fmt.Sprintf("%v", fio))
 	return fio, nil
 }
-
 
 func mapServiceFioToFioDTO(fio service.Fio) FioDataBaseDTO {
 	var fioDB FioDataBaseDTO
