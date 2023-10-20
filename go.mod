86d561d419e312bd4e35170f0740bb4481f8ac0c
liza <vinogradova.lizaveta@gmail.com>
2023-10-20 15:49:24 +0300

    fixed migrations and configs

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.2.0
 	github.com/segmentio/kafka-go v0.4.43
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.17.0
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,13 +1,5 @@
-CREATE TABLE IF NOT EXISTS ticks.ticks (
-	id BIGINT PRIMARY KEY auto_increment NOT NULL,
-	`timestamp` BIGINT NULL COMMENT 'Unixtime in milliseconds',
-	symbol VARCHAR(100) NULL COMMENT 'name of insrtruments',
-	best_bid DOUBLE NULL COMMENT 'the best sell offer',
-	best_ask DOUBLE NULL COMMENT 'the best bye offer'
-)
-
-CREATE TABLE FIO (
-    id SERIAL PRIMARY KEY auto_increment NOT NULL,
+CREATE TABLE IF NOT EXISTS fio (
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     patronymic VARCHAR(255),
